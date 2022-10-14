8574a7a87297533624c2ea63d1131ff672d359c3
Michael Lobato <mike.lobato.wft@gmail.com>
2022-10-14 08:06:12 -0700

    adds three functoins that pass testing

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,30 @@
-function nameMenuItem() {
 
+function nameMenuItem(food) {
+  return 'Delicious ' + food
 }
 
+//refer to line 42 and ask about parameter 1 "menuItemName"
+function createMenuItem(name, price, type) {
+  return {name, price, type}
+}
+
+function addIngredients(ingredient, ingredients =[]) {
+  if(!ingredients.includes(ingredient))
+  ingredients.push(ingredient)
+}
+
+
+//=================
+
+function formatPrice() {
+
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
