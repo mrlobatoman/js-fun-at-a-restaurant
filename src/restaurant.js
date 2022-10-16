b1f5b5599b7ebb59810f1ca5586206f06c656d0d
Michael Lobato <mike.lobato.wft@gmail.com>
2022-10-16 16:19:35 -0700

    Initial commit

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,21 @@
+function createRestaurant(restName, menus) {
+var restaurant = {
+  name: restName,
+  menus: {breakfast: [], lunch: [], dinner: []}
+}
+return restaurant
+}
+
+function addMenuItem(restaurant, addItem) {
+  restaurant.menus.lunch.push(addItem)
+return addItem
+}
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
